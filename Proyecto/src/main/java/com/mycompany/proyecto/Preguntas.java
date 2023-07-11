8dfd9ab9f7c7eca74abb5c598b63ee5a10603ea3
Jean Carlos Aguilar Tinoco <jcaguila@espol.edu.ec>
2023-07-10 20:44:54 -0500

    Creación de clase Pregunta

---
+++ Proyecto/src/main/java/com/mycompany/proyecto/Preguntas.java
@@ -0,0 +1,28 @@
+package com.mycompany.proyecto;
+import java.util.ArrayList;
+
+public class Pregunta{
+
+//Atributos:
+    private String enunciado;
+    private int nivel;
+    private ArrayList<String> listaRespuestas;
+    private String respuestaCorrecta;
+    private TipoComodin comodin;
+
+//Constructor:
+    public Pregunta(String e,int n,String rCorrecta, ArrayList<String> resp){
+        enunciado = e;
+        nivel = n;
+        listaRespuestas = resp;
+        respuestaCorrecta = rCorrecta;
+    }
+
+//Métodos:
+    public String IndicarRespuestaCorrecta(){
+        return respuestaCorrecta;
+    }
+public void agregarPregunta(String p){
+listaRespuestas.add(p)
+}
+}
