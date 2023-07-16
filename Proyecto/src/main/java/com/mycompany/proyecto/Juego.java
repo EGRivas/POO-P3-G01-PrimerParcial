1229a043918219f966f6e9758c9389b1f9ce6441
jamorett <jamorett@espol.edu.ec>
2023-07-16 10:21:19 -0500

    metodo empezarJuego

--- Proyecto/src/main/java/com/mycompany/proyecto/Juego.java
+++ Proyecto/src/main/java/com/mycompany/proyecto/Juego.java
@@ -49,10 +49,49 @@ public class Juego{
         }
         return null;
     }
-    public void empezarJuego(){
-        // * empieza el juego *
 
+    public void empezarJuego(Materia m) {
+
+        ArrayList<Preguntas> preguntasJuego = m.getPreguntas(); // Obtener las preguntas seleccionadas para el juego
+        int numPreguntas = preguntasJuego.size();
+        int preguntaActual = 0;
+
+        while (preguntaActual < numPreguntas) {
+            Pregunta pregunta = preguntasJuego.get(preguntaActual);
+            mostrarPregunta(pregunta);
+
+            // Obtener la respuesta ingresada por el participante
+            System.out.print("Ingrese la letra de la respuesta (A, B, C o D), o * para usar un comodín: ");
+            String respuestaIngresada = sc.nextLine().toUpperCase();
+
+            // Verificar si se usará un comodín
+            if (respuestaIngresada.equals("*")) {
+                mostrarComodinesDisponibles(); // Mostrar comodines disponibles (falta implementar)
+                continue; // Volver a mostrar la misma pregunta
+            }
+
+            // Verificar la respuesta ingresada
+            boolean respuestaCorrecta = pregunta.indicarRespuestaCorrecta(respuestaIngresada);
+            if (!respuestaCorrecta) {
+                System.out.println("Respuesta incorrecta. Fin del juego.");
+                return; // Terminar el juego si la respuesta es incorrecta
+            }
+
+            preguntaActual++;
+        }
+
+        // Si el estudiante ha llegado hasta aquí, ha respondido correctamente todas las preguntas
+        System.out.println("¡Felicitaciones! Has respondido correctamente a todas las preguntas.");
+
+        // Obtener el premio ingresado por el profesor
+        System.out.print("Ingrese el premio que has ganado: ");
+        String premio = sc.nextLine();
+
+        System.out.println("¡Felicidades! Has ganado: " + premio);
+
+        sc.close();
     }
+
 
 
 }
